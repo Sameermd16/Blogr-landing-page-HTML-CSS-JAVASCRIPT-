1a1e92cbfdd47333dc879218975813c19940c022
sameermd16 <sameerthemc@gmail.com>
2023-02-18 17:33:23 +0530

    JavaScript

---
+++ script.js
@@ -0,0 +1,35 @@
+const productBtn = document.getElementById("product-btn");
+const productDropdown = document.getElementById("product-dropdown");
+const companyBtn = document.getElementById("company-btn");
+const companyDropdown = document.getElementById("company-dropdown");
+const connectBtn = document.getElementById("connect-btn");
+const connectDropdown = document.getElementById("connect-dropdown");
+
+productBtn.addEventListener("click", () => {
+    if (productDropdown.classList.contains("opacity-none")){
+        productDropdown.classList.remove("opacity-none");
+        companyDropdown.classList.add("opacity-none");
+        connectDropdown.classList.add("opacity-none");
+    }else {
+        productDropdown.classList.add("opacity-none");
+    }
+})
+
+companyBtn.addEventListener("click", () => {
+    if (companyDropdown.classList.contains("opacity-none")){
+        companyDropdown.classList.remove("opacity-none");
+        productDropdown.classList.add("opacity-none");
+        connectDropdown.classList.add("opacity-none");
+    }else {
+        companyDropdown.classList.add("opacity-none");
+    }
+})
+connectBtn.addEventListener("click", () => {
+    if (connectDropdown.classList.contains("opacity-none")){
+        connectDropdown.classList.remove("opacity-none");
+        productDropdown.classList.add("opacity-none");
+        companyDropdown.classList.add("opacity-none");
+    }else{
+        connectDropdown.classList.add("opacity-none");
+    }
+})
